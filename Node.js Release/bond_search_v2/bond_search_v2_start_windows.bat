dfc20436087ed3f5b09859f7f83abe7310bb546b
Mikhail Shardin <43311539+empenoso@users.noreply.github.com>
2022-08-20 12:54:36 +0500

    Update bond_search_v2_start_windows.bat

--- Node.js Release/bond_search_v2/bond_search_v2_start_windows.bat
+++ Node.js Release/bond_search_v2/bond_search_v2_start_windows.bat
@@ -1,6 +1,12 @@
 @echo off
-node -v
-npm -v
-CD /d "D:\SynologyDrive\rabota\2018_investments\2020_04_node.js\bond_search_v2"
-node index.js
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%-%ldt:~10,2%
+echo Wait
+call node -v
+call npm -v
+echo Wait, under Windows %COMPUTERNAME% there is no process display
+echo Check "log_%ldt%.txt" in "log" folder AFTER closing this window
+echo Process started at %ldt%
+pushd D:\SynologyDrive\rabota\2018_investments\2020_04_node.js\bond_search_v2
+cmd /q /k node index.js > D:\SynologyDrive\rabota\2018_investments\2020_04_node.js\bond_search_v2\log\log_%ldt%.txt
 pause
